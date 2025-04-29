d08a96a6940fff5e534d8aac7ee902d8e239bc2b
iiTzX1sRol <vovavovakak@gmail.com>
2025-04-29 22:12:00 +0300

    refactor code

--- src/main/java/com/markelloww/projectmanagement/service/NewUserDetailsService.java
+++ src/main/java/com/markelloww/projectmanagement/service/NewUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.markelloww.projectmanagement.service;
 
-import com.markelloww.projectmanagement.model.User;
 import com.markelloww.projectmanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,13 +18,15 @@ public class NewUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .build();
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .map(user -> User.builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .accountExpired(false)
+                        .credentialsExpired(false)
+                        .accountLocked(false)
+                        .build())
+                .orElse(null);
     }
 }
